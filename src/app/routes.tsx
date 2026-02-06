0c8db9f37792537a8c0876e27c9299c79fa71721
HugoG68 <hugo.giolai@epitech.eu>
2026-02-06 17:57:31 +0100

    Feat: Remove useless data

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import Home from '../pages/Home/Home'
-import About from '../pages/About/About'
-import Contact from '../pages/Contact/Contact'
 
 const router = createBrowserRouter([
   {
@@ -10,8 +8,6 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'about', element: <About /> },
-      { path: 'contact', element: <Contact /> },
     ],
   },
 ])
